558266c3ad3b954cc31fc8995341d7e3bc55ae0f
MarceloDh <marceloducommunh@gmail.com>
2025-10-20 15:34:59 -0300

    Delete persona.py

--- persona.py
+++
@@ -1,6 +0,0 @@
-class Persona:
-    def __init__(self, nombre, direccion, telefono, email):
-        self._nombre = nombre
-        self._direccion = direccion
-        self._telefono = telefono
-        self._email = email
