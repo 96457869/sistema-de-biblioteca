44eb53e2c2dcfa8268c8cb5a2fba76c5dba23ec7
MarceloDh <marceloducommunh@gmail.com>
2025-10-20 15:04:27 -0300

    Create persona.py

---
+++ persona.py
@@ -0,0 +1,6 @@
+class Persona:
+    def __init__(self, nombre, direccion, telefono, email):
+        self._nombre = nombre
+        self._direccion = direccion
+        self._telefono = telefono
+        self._email = email
