745c2dd60e3028c457ddfba001ea8adc85ff0bad
MarceloDh <marceloducommunh@gmail.com>
2025-10-20 15:49:07 -0300

    Update libro.py

--- libro.py
+++ libro.py
@@ -1,12 +1,41 @@
 class Libro:
-    # le agregue 'fecha_publicacion' para guardar el año del libro
-    def __init__(self, titulo, autor, fecha_publicacion):
-        self.titulo = titulo
-        self.autor = autor
-        self.fecha_publicacion = fecha_publicacion
-        self.disponible = True
+    def __init__(self, titulo: str, autor: str, fecha_publicacion: int):
+        self._id_libro = None
+        self._titulo = titulo
+        self._autor = autor
+        self._fecha_publicacion = fecha_publicacion
+        self._disponible = True
+
+    @property
+    def id_libro(self):
+        return self._id_libro
+
+    @property
+    def titulo(self):
+        return self._titulo
+
+    @property
+    def autor(self):
+        return self._autor
     
+    @property
+    def fecha_publicacion(self):
+        return self._fecha_publicacion
+
+    @property
+    def disponible(self):
+        return self._disponible
+    
+    @id_libro.setter
+    def id_libro(self, id_libro):
+        self._id_libro = id_libro
+
+    def cambiar_estado(self, nuevo_estado: bool):
+        self._disponible = nuevo_estado
+
     def mostrar_info(self):
-        estado = "Disponible" if self.disponible else "Prestado"
-        # esto lo agregue porque lo que hace actualiza para mostrar también el año de publicación de los libros eso pense
-        return f"'{self.titulo}' por {self.autor} ({self.fecha_publicacion}) - {estado}"
+        estado = "Disponible" if self._disponible else "Prestado"
+        return f"ID: {self._id_libro} | '{self._titulo}' por {self._autor} ({self._fecha_publicacion}) - {estado}"
+
+    def __str__(self):
+        return self.mostrar_info()
